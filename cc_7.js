0f338a2c67131b7380f5c41ebbb00511ec6a6423
Sahara Sipple <sahara36@usf.edu>
2025-02-10 13:06:39 -0500

    Task 3 - Customer Loyalty Discount

--- cc_7.js
+++ cc_7.js
@@ -17,3 +17,16 @@ function calculateHourlyWage(salary, hoursPerWeek){ // declares function and its
 
 console.log(calculateHourlyWage(52000,40)); // Expected output: "Hourly Wage: $25.00"
 console.log(calculateHourlyWage(75000,35)); // Expected output: "Hourly Wage: $41.21"
+
+// Task 3: Arrow Function //
+
+const calculateLoyaltyDiscount = (amount, years) => { // defines the function and its variables
+    let discount = 0;  // declares discount variable
+    if (years >= 5) discount = (amount * .85).toFixed(2); // If years are >=5, the final discounted price is a 15% discount (amount * .85)
+    else if (years >= 3) discount = (amount * .9).toFixed(2); // If years are >= 3, final discounted price is 10% discount (amount * .9)
+    else discount = (amount * .95).toFixed(2); // for all other years (less than 3 years), final discounted price is 5% discount (amount * .95)
+    console.log(`Discounted Price: $${discount}`); // logs discounted price based off discount formula using template literal
+};
+
+calculateLoyaltyDiscount(100, 6); // Expected output: "Discounted Price: $85.00"
+calculateLoyaltyDiscount(200, 2); // Expected output: "Discounted Price: $190.00"
